5f624ebcd8683fba7c35fce367bd47e08e84b09d
Il'ya Allen <ilyaallen123@gmail.com>
2026-04-13 20:06:59 +1000

    Header Text Changeable

--- pokescript.py
+++ pokescript.py
@@ -1,1 +1,8 @@
+from pyscript import web, when
+
+# This is how you change the text of an item
+new_text = "Yes Boys"
+header_access = web.page["main_header"]
+header_access.innerText = new_text
+
 print("Hello World")
